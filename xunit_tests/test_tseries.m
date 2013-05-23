cdf2c19040772773f550195251d024eacc3cd789
mkjohnst <michaelkjohnston@users.noreply.github.com>
2013-05-23 10:51:35 +0200

    more tseries tests

--- xunit_tests/test_tseries.m
+++ xunit_tests/test_tseries.m
@@ -4,20 +4,105 @@ function test_tseries
 initTestSuite;
 end
 
-function test_sded(obj)
+function x=setup()
+x=tseries(qq(1,1):qq(3,2),sin(1:10));
+end
+
+function test_sded(~)
 x=tseries();
 assertEqual(startdate(x),NaN);
 assertEqual(enddate(x),NaN);
 end
 
-function test_assign(obj) %#ok<*DEFNU>
+function test_assign(~) %#ok<*DEFNU>
 x=tseries();
 x(1:5)=2:6;
 assertVectorsAlmostEqual(double(x),(2:6)');
 assertVectorsAlmostEqual(range(x),1:5);
 end
 
-function test_acf(obj)
-x=tseries(1:10,1:10);
-assertElementsAlmostEqual(acf(x),var(x));
+function test_subsindex(obj)
+assertVectorsAlmostEqual(obj(:),double(obj));
 end
+
+function test_detrend(obj)
+expected=[0
+   0.221770008206370
+  -0.392463844370859
+  -1.136442781550070
+  -1.184620994716695
+  -0.351168652063897
+   0.739177011042403
+   1.225492225135581
+   0.802196029942541
+  -0.000000000000001];
+assertVectorsAlmostEqual(double(detrend(obj)),expected);
+end
+
+function test_isscalar(~)
+assertEqual(isscalar(tseries(1,1)),true);
+assertEqual(isscalar(tseries(1:2,1)),true);
+assertEqual(isscalar(tseries(1,[1 2])),false);
+end
+
+function test_round(obj)
+assertVectorsAlmostEqual(double(round(obj)),round(double(obj)));
+end
+
+function test_bxsfun(obj)
+assertVectorsAlmostEqual(double(bsxfun(@max,obj,0)),bsxfun(@max,double(obj),0));
+end
+
+function test_acf(obj)
+assertElementsAlmostEqual(acf(obj),var(obj));
+end
+
+function test_mean(obj)
+assertElementsAlmostEqual(mean(obj),0.141118837121801);
+end
+
+function test_hpf(obj)
+expected=[0.332158721583314
+   0.287235347551190
+   0.242630293683581
+   0.199050668944550
+   0.157140138369643
+   0.116944958766749
+   0.077813846685613
+   0.038847793390374
+  -0.000490226884807
+  -0.040143170872499];
+assertVectorsAlmostEqual(double(hpf(obj)),expected);
+expected=[   0.509312263224583
+   0.622062079274492
+  -0.101510285623714
+  -0.955853164252478
+  -1.116064413032781
+  -0.396360456965675
+   0.579172752033176
+   0.950510453233008
+   0.412608712126564
+  -0.503877940016871];
+assertVectorsAlmostEqual(double(hpf2(obj)),expected);
+end
+
+function test_isempty(~)
+assertEqual(isempty(tseries()),true);
+assertEqual(isempty(tseries(1,1)),false);
+end
+
+function test_redate(obj)
+assertVectorsAlmostEqual(qq(2,2):qq(4,3),range(redate(obj,qq(1,1),qq(2,2))));
+end
+
+function test_dates(obj)
+assertEqual(char(dat2str(str2dat('01-2010','dateformat=','MM-YYYY','freq',12))),'2010M01');
+assertEqual(char(dat2str(str2dat('01-2010','dateformat=','MM-YYYY','freq',4))),'2010Q1');
+assertEqual(char(dat2str(str2dat('01-2010','dateformat=','MM-YYYY','freq',1))),'2010Y');
+assertEqual(char(dat2str(qq(2010,1))),'2010Q1');
+assertEqual(char(dat2str(mm(2010,1))),'2010M01');
+end
+
+
+
+
