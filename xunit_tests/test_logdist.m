9d488de0f81669324a9baf65426a7379b24760dd
mkjohnst <michaelkjohnston@users.noreply.github.com>
2013-06-12 11:14:21 +0200

    added some tests for logdist package to xunit_tests

---
+++ xunit_tests/test_logdist.m
@@ -0,0 +1,24 @@
+% test_logdist.m
+
+function test_logdist
+initTestSuite;
+end
+
+function test_normal_scalar %#ok<*DEFNU>
+if license('test','statistics_toolbox')
+    fh_sn = logdist.normal(0,1) ;
+    assertElementsAlmostEqual( normpdf(2), fh_sn(2,'pdf') ) ;
+    Mu = 1 ;
+    Sig = 2 ;
+    fh = logdist.normal(Mu,Sig) ;
+    assertElementsAlmostEqual( normpdf(2,Mu,Sig), fh(2,'pdf') ) ;
+end
+end
+
+function test_t_scalar
+if license('test','statistics_toolbox')
+    df = 10 ;
+    fh_st = logdist.normal(0, 1, df) ;
+    assertElementsAlmostEqual( tpdf(2,df), fh_st(2,'pdf') ) ;
+end
+end
