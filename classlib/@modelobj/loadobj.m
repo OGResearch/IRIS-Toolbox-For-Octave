b337a5022d2cb75e286799fb16e6e34801d9ac28
mkjohnst <michaelkjohnston@users.noreply.github.com>
2013-06-13 17:58:55 +0200

    new version of poster/regen, still doesn't work :)

---
+++ classlib/@modelobj/loadobj.m
@@ -0,0 +1,37 @@
+function This = loadobj(This)
+% loadobj  [Not a public function] Prepare modelobj for use in workspace and handle bkw compatibility.
+%
+% Backend IRIS function.
+% No help provided.
+
+% -IRIS Toolbox.
+% -Copyright (c) 2007-2013 IRIS Solutions Team.
+
+%--------------------------------------------------------------------------
+
+% Create empty aliases if missing.
+if isempty(This.namealias)
+    This.namealias = cell(size(This.name));
+    This.namealias(:) = {''};
+end
+if isempty(This.eqtnalias)
+    This.eqtnalias = cell(size(This.eqtn));
+    This.eqtnalias(:) = {''};
+end
+
+% Handle carry-around functions.
+if iscell(This.Export)
+    Export = struct('filename',{},'content',{});
+    for i = 1 : 2 : length(This.Export)
+        Export(end+1).filename = This.Export{i}; %#ok<AGROW>
+        Export(end).content = This.Export{i+1};
+    end
+    This.Export = Export;
+elseif isempty(This.Export)
+    This.Export = struct('filename',{},'content',{});
+end
+
+% Create and save carry-around files.
+export(This);
+
+end
