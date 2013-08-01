7253befe22d0c1e9d7e9c73e818c89da23159c3c
Jaromír Beneš <jaromir-benes@users.noreply.github.com>
2013-08-01 09:17:33 -0400

    Provide help on utils/findlast.m

--- +utils/findlast.m
+++ +utils/findlast.m
@@ -1,9 +1,48 @@
-function POS = findlast(X)
+function Pos = findlast(X)
+% findlast  Find the last non-zero or true value in 2nd dimension.
+%
+% Syntax
+% =======
+%
+%     Pos = utils.findlast(X);
+%
+% Input arguments
+% ================
+%
+% * `X` [ logical | numeric ] - A logical or numeric matrix
+% (3-dimensional at most).
+%
+% Output arguments
+% =================
+%
+% * `Pos` [ numeric ] - The 2nd-dimension position of the last `true` or
+% last non-zero value in any row or any page of `X`; returns `0` if non
+% `true` or non-zero value exists.
+%
+% Description
+% ============
+%
+% If `X` is a numeric array, all `NaN` values are first reset to zero
+% before looking up the position of the last non-zero value.
+%
+% Example
+% ========
+%
+% -IRIS Toolbox.
+% -Copyright (c) 2007-2013 IRIS Solutions Team.
+
+%--------------------------------------------------------------------------
+
 if isempty(X)
-    POS = 0;
-elseif isnumeric(X)
-    POS = max([0,find(any(any(X ~= 0,3),1),1,'last')]);
-else
-    POS = max([0,find(any(any(X,3),1),1,'last')]);
+    Pos = 0;
+    return
 end
+
+if ~islogica(X)
+    X(isnan(X)) = 0;
+    X = X ~= 0;
+end
+
+Pos = max([0,find(any(any(X,3),1),1,'last')]);
+
 end
