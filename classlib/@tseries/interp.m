42eb71031a18e9eb4049069af7a99f2134197276
Jaromír Beneš <jaromir-benes@users.noreply.github.com>
2013-09-27 09:04:28 -0400

    Update the default method name in tseries/interp to comply with Matlab R2013b+

--- classlib/@tseries/interp.m
+++ classlib/@tseries/interp.m
@@ -50,26 +50,26 @@ end
 
 %--------------------------------------------------------------------------
 
-if any(isinf(Range))
-   Range = get(X,'range');
+if isequal(Range,Inf)
+    Range = get(X,'range');
 elseif ~isempty(Range)
-   Range = Range(1) : Range(end);
-   X.data = rangedata(X,Range);
-   X.start = Range(1);
+    Range = Range(1) : Range(end);
+    X.data = rangedata(X,Range);
+    X.start = Range(1);
 else
-   X = empty(X);
-   return
+    X = empty(X);
+    return
 end
 
 data = X.data(:,:);
 grid = dat2grid(Range);
 grid = grid - grid(1);
 for i = 1 : size(data,2)
-   inx = ~isnan(data(:,i));
-   if any(~inx)
-      data(~inx,i) = interp1(...
-         grid(inx),data(inx,i),grid(~inx),opt.method,'extrap');   
-   end
+    inx = ~isnan(data(:,i));
+    if any(~inx)
+        data(~inx,i) = interp1(...
+            grid(inx),data(inx,i),grid(~inx),opt.method,'extrap');
+    end
 end
 
 X.data(:,:) = data;
