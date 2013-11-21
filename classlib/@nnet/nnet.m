2925a0c096af69e177a59e7ede37fc3050e8c3ee
mkjohnst <michaelkjohnston@users.noreply.github.com>
2013-11-21 16:09:13 +0200

    more progress on nnet/estimate(), still doesn't work

--- classlib/@nnet/nnet.m
+++ classlib/@nnet/nnet.m
@@ -37,6 +37,10 @@ classdef nnet < userdataobj & getsetobj
         nLayer ;
     end
     
+    methods( Static )
+        varargout = initConv(varargin) ;
+    end
+    
     methods
         function This = nnet(Inputs,Outputs,HiddenLayout,varargin)
             % -IRIS Toolbox.
@@ -69,28 +73,45 @@ classdef nnet < userdataobj & getsetobj
             This.OutputTransfer = options.OutputTransfer ;
             This.Type = options.Type ;
             
-            % *** Construct initial parameters
+            % *** Construct initial parameters ***
             myStruct = struct() ;
-            myStruct.Transfer = 1 ;
-            myStruct.Bias = 0 ;
-            myStruct.Weight = {} ;
+            
             % No weighting possible at input nodes
-            This.Params = cellfun(@(x) myStruct, cell(This.nLayer+2,1), 'UniformOutput', false) ;
-            This.Params{1}.Weight = NaN ;
+            This.Params ...
+                = cellfun(@(x) myStruct, cell(This.nLayer+2,1), 'UniformOutput', false) ;
+            This.Params{1}.Bias ...
+                = cellfun(any2func(options.initBias), cell(This.nInputs,1), 'UniformOutput', false) ;
+            This.Params{1}.Transfer ...
+                = cellfun(any2func(options.initTransfer), cell(This.nInputs,1), 'UniformOutput', false) ;
+            
             % First nodes weight inputs
-            initW = ones(This.nInputs,1) ;
+            initW = cell2mat(arrayfun(any2func(options.initWeight), ones(This.nInputs,1), 'UniformOutput', false)) ;
             This.Params{2}.Weight ...
                 = cellfun(@(x) initW, cell(This.HiddenLayout(1),1), 'UniformOutput', false) ;
+            This.Params{2}.Bias ...
+                = cellfun(any2func(options.initWeight), cell(This.HiddenLayout(1),1), 'UniformOutput', false) ;
+            This.Params{2}.Transfer ...
+                = cellfun(any2func(options.initTransfer), cell(This.HiddenLayout(1),1), 'UniformOutput', false) ;
+
             % Subsequent nodes weight outputs of previous layer
             for iLayer = 2:This.nLayer
                 initW = ones(This.HiddenLayout(iLayer-1),1) ; 
                 This.Params{iLayer+1}.Weight ...
                     = cellfun(@(x) initW, cell(This.HiddenLayout(iLayer),1), 'UniformOutput', false) ;
+                This.Params{iLayer+1}.Bias ...
+                    = cellfun(any2func(options.initWeight), cell(This.HiddenLayout(iLayer),1), 'UniformOutput', false) ;
+                This.Params{iLayer+1}.Transfer ...
+                    = cellfun(any2func(options.initTransfer), cell(This.HiddenLayout(iLayer),1), 'UniformOutput', false) ;
             end
+            
             % Output nodes weight outputs of last layer
             initW = ones(This.HiddenLayout(end),1) ;
             This.Params{end}.Weight ...
                 = cellfun(@(x) initW, cell(This.nOutputs,1), 'UniformOutput', false) ;
+            This.Params{end}.Bias ...
+                = cellfun(any2func(options.initBias), cell(This.nOutputs,1), 'UniformOutput', false) ;
+            This.Params{end}.Transfer ...
+                = cellfun(any2func(options.initTransfer), cell(This.nOutputs,1), 'UniformOutput', false) ;
         end
 
         varargout = disp(varargin) ;
