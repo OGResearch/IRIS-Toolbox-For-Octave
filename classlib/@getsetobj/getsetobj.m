7d4706df0979d5ded3535485aa8d9d53d51359e0
Jaromír Beneš <jaromir-benes@users.noreply.github.com>
2013-11-25 13:49:14 -0500

    Clean up.

--- classlib/@getsetobj/getsetobj.m
+++ classlib/@getsetobj/getsetobj.m
@@ -1,21 +1,26 @@
 classdef getsetobj
+    % getsetobj  [Not a public class] Helper class to handle get and set requests.
+    %
+    % Backend IRIS class.
+    % No help provided.
+    
+    % -IRIS Toolbox.
+    % -Copyright (c) 2007-2013 IRIS Solutions Team.
     
     properties
     end
     
     methods
-        
         function This = getsetobj(varargin)
         end
-        
+    end
+    
+    methods
         varargout = get(varargin)
-        
     end
     
     methods (Static,Access=protected,Hidden)
-        
         varargout = myalias(varargin)
-        
     end
     
 end
