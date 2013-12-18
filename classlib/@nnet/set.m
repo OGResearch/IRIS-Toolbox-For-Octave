5b9c9c106fa5de78125f57126943ade829b5a60b
mkjohnst <michaelkjohnston@users.noreply.github.com>
2013-12-18 21:03:02 +0200

    cleanup

--- classlib/@nnet/set.m
+++ classlib/@nnet/set.m
@@ -44,200 +44,126 @@ nArg = length(varargin);
 found = true(1,nArg);
 validated = true(1,nArg);
 for iArg = 1 : 2 : nArg
-    [found(iArg),validated(iArg)] = ...
-        doSet(lower(varargin{iArg}),varargin{iArg+1});
+	[found(iArg),validated(iArg)] = ...
+		doSet(lower(varargin{iArg}),varargin{iArg+1});
 end
 
 % Report queries that are not modifiable model object properties.
 if any(~found)
-    utils.error('nnet', ...
-        'This is not a modifiable neural network model object property: ''%s''.', ...
-        varargin{~found});
+	utils.error('nnet', ...
+		'This is not a modifiable neural network model object property: ''%s''.', ...
+		varargin{~found});
 end
 
 % Report values that do not pass validation.
 if any(~validated)
-    utils.error('nnet', ...
-        'The value for this property does not pass validation: ''%s''.', ...
-        varargin{~validated});
+	utils.error('nnet', ...
+		'The value for this property does not pass validation: ''%s''.', ...
+		varargin{~validated});
 end
 
 % Subfunctions.
 
 %**************************************************************************
-    function [Found,Validated,iLayer] = doSet(UsrQuery,Value)
-        
-        Found = true;
-        Validated = true;
-        query = nnet.myalias(UsrQuery);
-        
-        if isfunc(Value) || isnumericscalar(Value)
-            switch query
-                case 'activation'
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode} ...
-                                = set( This.Neuron{iLayer}{iNode}, 'activation', Value ) ;
-                        end
-                    end
-                    
-                case 'activationbounds'
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode} ...
-                                = set( This.Neuron{iLayer}{iNode}, 'activationbounds', Value ) ;
-                        end
-                    end
-                    
-                case 'output'
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode} ...
-                                = set( This.Neuron{iLayer}{iNode}, 'output', Value ) ;
-                        end
-                    end
-                    
-                case 'outputbounds'
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode} ...
-                                = set( This.Neuron{iLayer}{iNode}, 'outputbounds', Value ) ;
-                        end
-                    end
-                    
-                case 'hyper'
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode} ...
-                                = set( This.Neuron{iLayer}{iNode}, 'hyper', Value ) ;
-                        end
-                    end
-                    
-                case 'hyperbounds'
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode} ...
-                                = set( This.Neuron{iLayer}{iNode}, 'hyperbounds', Value ) ;
-                        end
-                    end
-                    
-                case 'bounds'
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode} ...
-                                = set( This.Neuron{iLayer}{iNode}, 'bounds', Value ) ;
-                        end
-                    end
-                    
-                case 'param'
-                    This = set( This, 'activation', Value ) ;
-                    This = set( This, 'output', Value ) ;
-                    This = set( This, 'hyper', Value ) ;
+	function [Found,Validated,iLayer] = doSet(UsrQuery,Value)
+		
+		Found = true;
+		Validated = true;
+		query = nnet.myalias(UsrQuery);
+		
+		if isfunc(Value) || isnumericscalar(Value)
+			switch query
+				case 'activation'
+					xxLayerNodeLoop('activation',Value) ;
+					
+				case 'activationbounds'
+					xxLayerNodeLoop('activationbounds',Value) ;
+					
+				case 'output'
+					xxLayerNodeLoop('output',Value) ;
+					
+				case 'outputbounds'
+					xxLayerNodeLoop('outputbounds',Value) ;
+					
+				case 'hyper'
+					xxLayerNodeLoop('hyper',Value) ;
+					
+				case 'hyperbounds'
+					xxLayerNodeLoop('hyperbounds',Value) ;
+					
+				case 'bounds'
+					xxLayerNodeLoop('bounds',Value) ;
+					
+				case 'param'
+					This = set( This, 'activation', Value ) ;
+					This = set( This, 'output', Value ) ;
+					This = set( This, 'hyper', Value ) ;
+					
+				case 'userdata'
+					This = userdata( This, Value );
+					
+				otherwise
+					Found = false;
+					
+			end
+		else
+			% Value is a vector
+			switch query
+				case 'activation'
+					xxLayerNodeIndexLoop('ActivationParams','ActivationIndex',Value) ;
+					
+				case 'activationBounds'
+					xxLayerNodeIndexLoop('ActivationBounds','ActivationIndex',Value) ;
+					
+				case 'output'
+					xxLayerNodeIndexLoop('OutputParams','OutputIndex',Value) ;
+					
+				case 'outputBounds'
+					xxLayerNodeIndexLoop('OutputBounds','OutputIndex',Value) ;
+					
+				case 'hyper'
+					xxLayerNodeIndexLoop('HyperParams','HyperIndex',Value) ;
 
-                case 'userdata'
-                    This = userdata( This, Value );
-                    
-                otherwise
-                    Found = false;
-                    
-            end
-        else
-            % Value is a vector
-            switch query
-                case 'activation'
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode}.ActivationParams ...
-                                = Value( This.Neuron{iLayer}{iNode}.ActivationIndex ) ;
-                        end
-                    end
-                    
-                case 'activationBounds'
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode}.ActivationBounds ...
-                                = Value( This.Neuron{iLayer}{iNode}.ActivationIndex ) ;
-                        end
-                    end
-                    
-                case 'output'
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode}.OutputParams ...
-                                = Value( This.Neuron{iLayer}{iNode}.OutputIndex ) ;
-                        end
-                    end
-                    
-                case 'outputBounds'
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode}.OutputBounds ...
-                                = Value( This.Neuron{iLayer}{iNode}.OutputIndex ) ;
-                        end
-                    end
-                    
-                case 'hyper'
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode}.HyperParams ...
-                                = Value( This.Neuron{iLayer}{iNode}.HyperIndex ) ;
-                        end
-                    end
-                    
-                case 'hyperBounds'
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode}.HyperBounds ...
-                                = Value( This.Neuron{iLayer}{iNode}.HyperIndex ) ;
-                        end
-                    end
-                    
-                case 'param'
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode}.ActivationParams ...
-                                = Value( This.Neuron{iLayer}{iNode}.ActivationIndex ) ;
-                        end
-                    end
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode}.HyperParams ...
-                                = Value( This.nActivationParams+This.Neuron{iLayer}{iNode}.HyperIndex ) ;
-                        end
-                    end
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode}.OutputParams ...
-                                = Value( This.nActivationParams+This.nHyperParams+This.Neuron{iLayer}{iNode}.OutputIndex ) ;
-                        end
-                    end
+				case 'hyperBounds'
+					xxLayerNodeIndexLoop('HyperBounds','HyperIndex',Value) ;
 
-                case 'bounds'
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode}.ActivationBounds ...
-                                = Value( This.Neuron{iLayer}{iNode}.ActivationIndex ) ;
-                        end
-                    end
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode}.HyperBounds ...
-                                = Value( This.nActivationParams+This.Neuron{iLayer}{iNode}.HyperIndex ) ;
-                        end
-                    end
-                    for iLayer = 1:This.nLayer+1
-                        for iNode = 1:numel(This.Neuron{iLayer})
-                            This.Neuron{iLayer}{iNode}.OutputBounds ...
-                                = Value( This.nActivationParams+This.nHyperParams+This.Neuron{iLayer}{iNode}.OutputIndex ) ;
-                        end
-                    end
-                    
-                otherwise
-                    Found = false ;
-                    
-            end
-        end
-        
-    end % doSet().
+					
+				case 'param'
+					xxLayerNodeIndexLoop('ActivationParams','ActivationIndex',Value) ;
+					xxLayerNodeIndexLoop('HyperParams','HyperIndex',Value,This.nActivationParams) ;
+					xxLayerNodeIndexLoop('OutputParams','OutputIndex',Value,This.nActivationParams+This.nHyperParams) ;
+					
+				case 'bounds'
+					xxLayerNodeIndexLoop('ActivationBounds','ActivationIndex',Value) ;
+					xxLayerNodeIndexLoop('HyperBounds','HyperIndex',Value,This.nActivationParams) ;
+					xxLayerNodeIndexLoop('OutputBounds','OutputIndex',Value,This.nActivationParams+This.nHyperParams) ;
+					
+				otherwise
+					Found = false ;
+					
+			end
+		end
+		
+	end % doSet().
 
+	function xxLayerNodeLoop(str,Value)
+		for iLayer = 1:This.nLayer+1
+			for iNode = 1:numel(This.Neuron{iLayer})
+				This.Neuron{iLayer}{iNode} ...
+					= set( This.Neuron{iLayer}{iNode}, str, Value ) ;
+			end
+		end
+	end
+
+	function xxLayerNodeIndexLoop(name,indexName,Value,offset)
+		if nargin<4
+			offset = 0 ;
+		end
+		for iLayer = 1:This.nLayer+1
+			for iNode = 1:numel(This.Neuron{iLayer})
+				This.Neuron{iLayer}{iNode}.(name) ...
+					= Value( This.Neuron{iLayer}{iNode}.(indexName) + offset ) ;
+			end
+		end
+	end
 end
