972ffb11f5d0217885c528726fb4460f22d5a7cf
sergey plotnikov <nul0m@users.noreply.github.com>
2013-12-20 10:23:26 +0100

    utf16->utf8 charcode converter simplified; utf8->utf16 added

--- utils/highCharCode2utf8.m
+++ utils/highCharCode2utf8.m
@@ -1,10 +1,7 @@
 function utf8code = highCharCode2utf8(hcc)
-  if hcc <= 255
+  if hcc <= 255 % octave's max char*1 code is 255
     utf8code = hcc;
   else
-    utf8code = nan(1,2);
-    b = dec2bin(hcc);
-    utf8code(1) = bin2dec(['110' repmat('0',1,5-length(b(1:end-6))) b(1:end-6)]);
-    utf8code(2) = bin2dec(['10' b(end-5:end)]);
+    utf8code = [fix(hcc/64) rem(hcc,64)] + [192 128];
   end
 end
