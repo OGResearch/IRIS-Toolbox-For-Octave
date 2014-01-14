3a043329684338310b7b103a6e09c12bb93a8aeb
sergey plotnikov <nul0m@users.noreply.github.com>
2014-01-14 20:29:03 +0100

    first two tests were added to the set of classdef issues tests

---
+++ -test_i4o_issues/testClassdef/test_overloadOctaveMethodByUserMethod.m
@@ -0,0 +1,17 @@
+% create two quarterly time series (objects of class [tseries])
+ts1 = tseries(qq(1,1):qq(2,4),1:8);
+ts2 = tseries(qq(1,1):qq(2,4),8:-1:1);
+
+% try to sum these time series, checking if plus() method of [tseries] class
+% overloads regular '+' operator
+try
+  tsc = ts1 + ts2;
+  clear ts1 ts2 tsc
+catch err
+  clear ts1 ts2 tsc
+  if strncmp(err.message,'binary operator ''+'' not implemented',35)
+    error('expected error:: plus() method of [tseries] is unable to overload Octave''s ''+'' operator');
+  else
+    rethrow(err);
+  end
+end
