74d7573d1007b57e37eb2abe6373c1639476ec50
sergey plotnikov <nul0m@users.noreply.github.com>
2014-01-21 20:35:22 +0100

    test renamed to mirror existing issue

--- -test_i4o_issues/testClassdef/test_overloadOctaveEndOperatorDeclaredInClassdefFile.m
+++ -test_i4o_issues/testClassdef/test_overloadOctaveEndOperatorDeclaredInClassdefFile.m
@@ -5,7 +5,7 @@ try
 catch err
   clear aa
   if strncmp(err.message,'parse error near line 11',24)
-    error('expected error:: ''end'' indexing operator cannot be overloaded');
+    error('expected error:: ''end'' indexing operator cannot be overloaded when declared in classdef file');
   else
     rethrow(err);
   end
