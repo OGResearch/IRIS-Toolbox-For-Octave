270f50a0b47eb8f0ecefb72e4d776515085c78be
sergey plotnikov <nul0m@users.noreply.github.com>
2014-01-21 20:38:08 +0100

    new test to check subsasgn() overloading when this possibility is added

---
+++ -test_i4o_issues/testClassdef/test_overloadSubsasgn.m
@@ -0,0 +1,15 @@
+ts1 = tseries(qq(1,1):qq(2,4),1:8);
+keyboard
+try
+  ts1{qq(2,2)} = 5555;
+  assert(ts1(qq(2,2))==5555,'tseries::subsasgn works wrong, value was assigned incorrectly');
+  clear ts1
+catch err
+  clear ts1
+  if strncmp(err.message,'subsasgn: object cannot be index with',37)
+    error('expected error:: subsasgn() method cannot be overloaded');
+  else
+    rethrow(err);
+  end
+  rethrow(err);
+end
