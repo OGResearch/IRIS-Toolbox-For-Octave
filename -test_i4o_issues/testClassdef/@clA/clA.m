281de98d4912b9df8259f0624ed4ba5654716929
sergey plotnikov <nul0m@users.noreply.github.com>
2014-01-23 09:39:11 +0100

    wrong syntax in a test case fixed

--- -test_i4o_issues/testClassdef/@clA/clA.m
+++ -test_i4o_issues/testClassdef/@clA/clA.m
@@ -1,15 +1,28 @@
 classdef clA
-
-    methods
-        function this = clA(varargin)
-            this = [1 2 3];
-        end
+  
+  properties
+    data = [];
+  end
+  
+  methods
+    function this = clA(varargin)
+      this.data = [1 2 3];
     end
-
-
-    methods (Hidden)
-        function index = end (this,k,n)
-            index = size(this,k);
-        end
+    function varargout = subsref(this,s)
+      varargout{1} = builtin('subsref',this.data,s);
     end
+    function disp(this)
+      disp(this.data)
+    end
+  end
+    
+  methods (Hidden)
+    function index = end(this,k,n)
+      if n==1
+        index = numel(this.data);
+      else
+        index = size(this.data,k);
+      end
+    end
+  end
 end
