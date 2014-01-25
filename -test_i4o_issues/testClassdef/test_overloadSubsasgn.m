3e7ed4b95728a645c0bd10108f91b2f90de76e5a
sergey plotnikov <nul0m@users.noreply.github.com>
2014-01-25 18:51:50 +0100

    test case syntax changed to work with current iris version

--- -test_i4o_issues/testClassdef/test_overloadSubsasgn.m
+++ -test_i4o_issues/testClassdef/test_overloadSubsasgn.m
@@ -1,7 +1,7 @@
 ts1 = tseries(qq(1,1):qq(2,4),1:8);
 
 try
-  ts1{qq(2,2)} = 5555;
+  ts1(qq(2,2)) = 5555;
   assert(ts1(qq(2,2))==5555,'tseries::subsasgn works wrong, value was assigned incorrectly');
   clear ts1
 catch err
