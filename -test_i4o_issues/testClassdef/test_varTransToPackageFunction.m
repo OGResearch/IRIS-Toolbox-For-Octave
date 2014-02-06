902fdc5264fcc8b670cc62cc3e3ee09419bd74f6
sergey plotnikov <nul0m@users.noreply.github.com>
2014-02-06 20:07:22 +0100

    new tests was added

---
+++ -test_i4o_issues/testClassdef/test_varTransToPackageFunction.m
@@ -0,0 +1,11 @@
+ts = tseries(qq(2010,1:18),@(n)randn(n,5));
+
+try
+  barcon(ts)
+catch err
+if ~isempty(strfind(err.message,'''h'' undefined near line'))
+    error('expected error:: variable value did not reach package function');
+  else
+    rethrow(err);
+  end
+end
