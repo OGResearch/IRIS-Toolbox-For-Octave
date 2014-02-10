6f168a84842f4a891230c2d00e10a12f85593516
sergey plotnikov <nul0m@users.noreply.github.com>
2014-02-10 18:45:48 +0100

    ndprop() upgraeded to work in Octave (which has no arrays of properties)

--- +utils/ndprop.m
+++ +utils/ndprop.m
@@ -19,11 +19,20 @@ try
     inx = ~[mc.PropertyList.Dependent];
     List = {mc.PropertyList(inx).Name};
 catch %#ok<CTCH>
-    % Compatibility with 2010b.
-    p = [mc.Properties{:}];
-    inx = ~[p.Dependent];
-    List = {p.Name};
-    List = List(inx);
+    try % Compatibility with 2010b.
+        p = [mc.Properties{:}];
+        inx = ~[p.Dependent];
+        List = {p.Name};
+        List = List(inx);
+    catch % Compatibility with Octave.
+        List = [];
+        prList = mc.PropertyList;
+        for px = 1:length(prList)
+          if ~prList{px}.Dependent
+            List = [List, {prList{px}.Name}];
+          end
+        end
+    end
 end
 
 end
