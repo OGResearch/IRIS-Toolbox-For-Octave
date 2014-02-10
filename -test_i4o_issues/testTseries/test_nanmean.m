331d8440eb4381f8a5c67878c8534d95cb494ab1
sergey plotnikov <nul0m@users.noreply.github.com>
2014-02-10 16:30:52 +0100

    new test for tseries class method. supposed to show Octave's lack of @classname.static_method_name() syntax for creating handles

---
+++ -test_i4o_issues/testTseries/test_nanmean.m
@@ -0,0 +1,15 @@
+absTol = eps()^(2/3);
+
+x = tseries(qq(1,1):qq(3,2),sin(1:10)) ;
+x(qq(1,3)) = NaN;
+
+try
+  assert(mean(x), NaN, absTol) ;
+  assert(nanmean(x), 0.141118707017571, absTol) ;
+catch err
+  if ~isempty(strfind(err.message,'function handle cannot be indexed with .'))
+    error('expected error:: dot-syntax is not allowed when creating handles');
+  else
+    rethrow(err);
+  end
+end
