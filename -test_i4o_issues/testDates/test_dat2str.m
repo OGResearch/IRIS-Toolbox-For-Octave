8267305327ae80a072b5f210068fc85f795a9e25
sergey plotnikov <nul0m@users.noreply.github.com>
2014-02-12 18:23:06 +0100

    * files were renamed to be included into test suite * assert is substituted with myassert to have it running well on Matlab

--- -test_i4o_issues/testDates/test_dat2str.m
+++ -test_i4o_issues/testDates/test_dat2str.m
@@ -1,7 +1,7 @@
 observ = dat2char(qq(2010,1)) ;
 expect = '2010Q1' ;
-assert(observ, expect) ;
+myassert(observ, expect) ;
 
 observ = dat2char(mm(2010,1)) ;
 expect = '2010M01' ;
-assertEqual(This, actual, expected) ;
+myassert(observ, expect) ;
--- -test_i4o_issues/testDates/test_str2dat.m
+++ -test_i4o_issues/testDates/test_str2dat.m
@@ -1,15 +1,15 @@
 observ = dat2char(str2dat('01-2010', ...
     'dateformat=', 'MM-YYYY', 'freq=', 12)) ;
 expect = '2010M01' ;
-assert(observ, expect) ;
+myassert(observ, expect) ;
 
 observ = dat2char(str2dat('01-2010', ...
     'dateformat=', 'MM-YYYY', 'freq=', 4)) ;
 expect = '2010Q1' ; 
-assert(observ, expect) ;
+myassert(observ, expect) ;
 
 observ = dat2char(str2dat('01-2010', ...
     'dateformat=', 'MM-YYYY', 'freq=', 1)) ;
 expect = '2010Y' ;
-assert(observ, expect);
+myassert(observ, expect);
 
