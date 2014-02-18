f6c97a409d913f27669717df57c0f2d37fce084b
Jaromír Beneš <jaromir-benes@users.noreply.github.com>
2014-02-18 14:40:24 -0500

    Streamline PDF publication of tutorials.

---
+++ +latex/howtopublish.m
@@ -0,0 +1,6 @@
+%% Publish M-Files to PDFs
+%
+% The following commands can be used to create PDF versions of the model
+% file and the m-files:
+%
+%     latex.publish('read_me_first.m',[],'evalCode=',false);
