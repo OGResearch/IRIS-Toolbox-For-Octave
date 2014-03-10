65f67219b95f1042d30947d5234e9eaa9e00edf7
Jaromír Beneš <jaromir-benes@users.noreply.github.com>
2014-03-10 17:13:19 -0400

    Cleanup.

--- classlib/@model/ismissing.m
+++ classlib/@model/ismissing.m
@@ -46,4 +46,6 @@ function [Flag,List] = ismissing(This,Inp,Range)
 [~,~,List] = datarequest('init',This,Inp,Range);
 Flag = ~isempty(List);
 
+% TODO: Add check for missing exogenized variables...
+
 end
