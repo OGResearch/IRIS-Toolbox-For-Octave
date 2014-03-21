9d4cc1d638aa1bc0b9cf09533acc2663e6fc9112
sergey plotnikov <nul0m@users.noreply.github.com>
2014-03-21 16:58:37 +0100

    test case showing that builtin('subsasgn', ..) cannot access protected property of a class even if called from within the class'es own subsasgn().

--- -test_i4o_issues/testClassdef/@clA/clA.m
+++ -test_i4o_issues/testClassdef/@clA/clA.m
@@ -4,12 +4,31 @@ classdef clA
     data = [];
   end
   
+  properties (GetAccess=public,SetAccess=protected,Hidden)
+    dataProt = [];
+  end
+  
   methods
     function this = clA(varargin)
       this.data = [1 2 3];
+      this.dataProt = [];
     end
     function varargout = subsref(this,s)
-      varargout{1} = builtin('subsref',this.data,s);
+      switch s.type
+        case {'{}','()'}
+          varargout{1} = builtin('subsref',this.data,s);
+        otherwise
+          varargout{1} = builtin('subsref',this,s);
+      end
+    end
+    function this = subsasgn(this,s,y)
+      switch s.type
+        case {'{}','()'}
+          this = builtin('subsasgn',this.data,s,y);
+        otherwise
+          this = builtin('subsasgn',this,s,y);
+          %this.dataProt = y;
+      end
     end
     function disp(this)
       disp(this.data)
