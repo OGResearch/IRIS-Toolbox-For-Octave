5fb3daa29a73701a59fd453cc920f0d322d3a3e5
sergey plotnikov <nul0m@users.noreply.github.com>
2014-03-21 16:13:17 +0100

    extra tests of dat2str() and str2dat() functionality

--- -test_i4o_issues/testDates/test_str2dat.m
+++ -test_i4o_issues/testDates/test_str2dat.m
@@ -1,15 +1,19 @@
 observ = dat2char(str2dat('01-2010', ...
-    'dateformat=', 'MM-YYYY', 'freq=', 12)) ;
+    'dateFormat=', 'MM-YYYY', 'freq=', 12)) ;
 expect = '2010M01' ;
 myassert(observ, expect) ;
 
 observ = dat2char(str2dat('01-2010', ...
-    'dateformat=', 'MM-YYYY', 'freq=', 4)) ;
+    'dateFormat=', 'MM-YYYY', 'freq=', 4)) ;
 expect = '2010Q1' ; 
 myassert(observ, expect) ;
 
 observ = dat2char(str2dat('01-2010', ...
-    'dateformat=', 'MM-YYYY', 'freq=', 1)) ;
+    'dateFormat=', 'MM-YYYY', 'freq=', 1)) ;
 expect = '2010Y' ;
-myassert(observ, expect);
+myassert(observ, expect) ;
 
+observ = dat2char(str2dat('2001-12-31', ...
+    'dateFormat=', '$YYYY-MM-DD','freq=', 52)) ;
+expect = '2002W01';
+myassert(observ, expect) ;
