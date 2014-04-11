938ecf2a38e434b90543c3a04bb7b799091cde9d
sergey plotnikov <nul0m@users.noreply.github.com>
2014-04-11 19:23:01 +0200

    bugs in test class were fixed

--- -test_i4o_issues/testClassdef/@subDouble/subDouble.m
+++ -test_i4o_issues/testClassdef/@subDouble/subDouble.m
@@ -2,9 +2,11 @@ classdef subDouble < double
 
 methods
 
-  function this = subDouble(varargin)
-    this = this@double(varargin);
-    this = 'n';
+  function this = subDouble(num,varargin)
+      if nargin == 0
+          num = [];
+      end
+      this = this@double(num);
   end
   
 end
