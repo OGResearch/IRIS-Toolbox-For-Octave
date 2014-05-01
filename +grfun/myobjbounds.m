4cf0569279b2043544a79937634eee04f6e87239
sergey plotnikov <nul0m@users.noreply.github.com>
2014-05-01 12:14:32 +0200

    myobjbounds upgraded: * to work with axes hadles * to ignore xlim and ylim when options 'xliminclude' and/or 'yliminclude' are 'off'

--- +grfun/myobjbounds.m
+++ +grfun/myobjbounds.m
@@ -12,21 +12,38 @@ catch
         types = {types};
     end
     
+    [this,types] = xxReplaceAxesWithKids(this,types);
+    
     for ix = 1:numel(types)
         switch types{ix}
             case {'line', 'surface'}
-                xData = get(this(ix), 'xData');
-                yData = get(this(ix), 'yData');
-                
+                if strcmpi(get(this(ix),'xliminclude'),'on')
+                    xData = get(this(ix), 'xData');
+                else
+                    xData = NaN;
+                end
+                if strcmpi(get(this(ix),'yliminclude'),'on')
+                    yData = get(this(ix), 'yData');
+                else
+                    yData = NaN;
+                end
             case 'patch'
                 xyData = get(this(ix), 'vertices');
                 fcs = get(this(ix), 'faces');
                 xyData = xyData(fcs(isfinite(fcs)),:);
-                xData = xyData(:,1);
-                yData = xyData(:,2);
+                if strcmpi(get(this(ix),'xliminclude'),'on')
+                    xData = xyData(:,1);
+                else
+                    xData = NaN;
+                end
+                if strcmpi(get(this(ix),'yliminclude'),'on')
+                    yData = xyData(:,2);
+                else
+                    yData = NaN;
+                end
             otherwise
-                xData = nan;
-                yData = nan;
+                xData = NaN;
+                yData = NaN;
         end
         
         xlim = [min(xlim(1), min(xData)),max(xlim(2), max(xData))];
@@ -36,4 +53,21 @@ catch
     lims = [xlim, ylim, 0, 0];
 end
 
+end
+
+function [this,types] = xxReplaceAxesWithKids(this,types)
+    axIx = strcmpi(types,'axes');
+    if all(~axIx)
+        return
+    end
+    newKids = [];
+    newTypes = [];
+    for ix = find(axIx)
+        allObj = findobj(this(ix));
+        newKids = [newKids;allObj(2:end)];
+        newTypes = get(newKids,'type');
+        [newKids,newTypes] = xxReplaceAxesWithKids(newKids,newTypes);
+    end
+    this = [this(~axIx);newKids];
+    types = [types(~axIx);newTypes];
 end
