09b7eb27daeace2288a11136e4914358db2017cd
sergey plotnikov <nul0m@users.noreply.github.com>
2014-05-13 12:02:42 +0200

    myassert() was upgraded to test structures in Matlab

--- -test_i4o_issues/myassert.m
+++ -test_i4o_issues/myassert.m
@@ -10,6 +10,22 @@ else
   end
   if ischar(expect) || iscellstr(expect)
     assert(strcmp(observ, expect));
+  elseif isstruct(expect)
+    fldsE = fieldnames(expect);
+    fldsO = fieldnames(observ);
+    inEonly = setdiff(expect,observ);
+    if ~isempty(inEonly)
+      error(['These fields are missing in Observed: ', ...
+        repmat('%s,',1,length(inEonly)),'\b'],inEonly{:});
+    end
+    inOonly = setdiff(observ,expect);
+    if ~isempty(inOonly)
+      error(['These fields are missing in Expected: ', ...
+        repmat('%s,',1,length(inOonly)),'\b'],inOonly{:});
+    end
+    for ix = 1 : length(fldsE)
+      myassert(observ.(fldsE{ix}),expect.(fldsE{ix}),absTol);
+    end
   elseif any(isnan(expect)) || any(isinf(expect))
     assert(isequaln(observ,expect));
   else
