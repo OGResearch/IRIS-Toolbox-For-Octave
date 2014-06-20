cdce0b7a21df0fa6e31495e1e989eb99a1d55ead
sergey plotnikov <nul0m@users.noreply.github.com>
2014-06-20 18:44:57 +0200

    @getsetobj/proplist was adjusted to have Octave specific content of removed ndprop()

--- classlib/@getsetobj/proplist.m
+++ classlib/@getsetobj/proplist.m
@@ -24,11 +24,21 @@ try
     inx = ~[mc.PropertyList.Dependent];
     List = {mc.PropertyList(inx).Name};
 catch %#ok<CTCH>
-    % Compatibility with R2010b.
-    p = [mc.Properties{:}];
-    inx = ~[p.Dependent];
-    List = {p.Name};
-    List = List(inx);
+    try
+        % Compatibility with R2010b.
+        p = [mc.Properties{:}];
+        inx = ~[p.Dependent];
+        List = {p.Name};
+        List = List(inx);
+    catch % Compatibility with Octave.
+        List = [];
+        prList = mc.PropertyList;
+        for px = 1:length(prList)
+          if ~prList{px}.Dependent
+            List = [List, {prList{px}.Name}]; %#ok<AGROW>
+          end
+        end
+    end
 end
 
 end
