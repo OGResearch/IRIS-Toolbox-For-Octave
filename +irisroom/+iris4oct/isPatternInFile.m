95bb29f223cccca21ed80d561eaf2aa2b8df7001
sergey plotnikov <nul0m@users.noreply.github.com>
2014-06-27 09:42:31 +0200

    new parsers/code-checkers, isPatternInFile improved, parser adjusting @is.bla-bla was improved

--- +irisroom/+iris4oct/isPatternInFile.m
+++ +irisroom/+iris4oct/isPatternInFile.m
@@ -5,16 +5,16 @@ flag = false;
 fid = fopen(filename,'r+');
 
 tline = ''; %#ok<NASGU>
-matchedLines = struct();
+matchedLines = struct('str',{},'n',[]);
 n = 1;
 while ~feof(fid)
     tline = fgetl(fid);
     out = regexp(tline,pattern,'once');
     if ~isempty(out)
         flag = true;
-        if n == 1
-            matchedLines.str = {tline};
-            matchedLines.n = n;
+        if isempty(matchedLines)
+            matchedLines(1).str = {tline};
+            matchedLines(1).n = n;
         else
             matchedLines.str = [matchedLines.str;tline];
             matchedLines.n = [matchedLines.n;n];
