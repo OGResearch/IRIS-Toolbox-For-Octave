3783d83d968b1ab0cb9343e3a12fc4ff3c6f065a
sergey plotnikov <nul0m@users.noreply.github.com>
2014-07-06 17:53:13 +0200

    temporary solution while @config is in IRIS

---
+++ utils/config.m
@@ -0,0 +1,13 @@
+function varargout = config(varargin)
+% config  [Not a public function] Dummy function needed for Octave version
+% of IRIS.
+%
+% Backend IRIS function.
+% No help provided.
+
+% -IRIS Toolbox.
+% -Copyright (c) 2007-2014 IRIS Solutions Team.
+
+%--------------------------------------------------------------------------
+
+end
