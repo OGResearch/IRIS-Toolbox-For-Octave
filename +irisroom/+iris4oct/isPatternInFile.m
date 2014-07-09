ccf7133cb3222e7aa16aeac683895015260f5741
sergey plotnikov <nul0m@users.noreply.github.com>
2014-07-09 10:46:13 +0200

    * extra feature in pattern searcher * new pattern search

--- +irisroom/+iris4oct/isPatternInFile.m
+++ +irisroom/+iris4oct/isPatternInFile.m
@@ -1,4 +1,8 @@
-function [flag, matchedLines] = isPatternInFile (filename, pattern)
+function [flag, matchedLines] = isPatternInFile (filename, pattern, nforward)
+
+if nargin<3
+    nforward = 0;
+end
 
 flag = false;
 
@@ -11,14 +15,23 @@ while ~feof(fid)
     tline = fgetl(fid);
     out = regexp(tline,pattern,'once');
     if ~isempty(out)
+        spos = ftell(fid);
         flag = true;
         if isempty(matchedLines)
             matchedLines(1).str = {tline};
             matchedLines(1).n = n;
+            matchedLines(1).fwd = {{}};
         else
             matchedLines.str = [matchedLines.str;tline];
             matchedLines.n = [matchedLines.n;n];
+            matchedLines.fwd{end+1} = {};
         end
+        fwline = '';
+        for i = 1 : nforward
+            fwline = fgetl(fid);
+            matchedLines.fwd{end} = [matchedLines.fwd{end};fwline];
+        end
+        fseek(fid,spos,'bof');
     end
     n = n + 1;
 end
--- +irisroom/+iris4oct/some_checks_and_parser_calls.m
+++ +irisroom/+iris4oct/some_checks_and_parser_calls.m
@@ -16,7 +16,7 @@ for ix = 1 : numel(lst)
     end
 end
 %}
-return
+% return
 
 %% kick inputParser workaround out of iris_clone
 %{
@@ -33,7 +33,7 @@ for ix = 1 : numel(files)
     irisroom.iris4oct.parseFile4inputParser_back(files{ix});
 end
 %}
-return
+% return
 
 %% look for char( in stable iris
 %{
@@ -51,7 +51,7 @@ for ix = 1 : numel(lst)
     end
 end
 %}
-return
+% return
 
 %% look for $0
 %{
@@ -69,7 +69,7 @@ for ix = 1 : numel(lst)
     end
 end
 %}
-return
+% return
 
 %% look for cd(
 %{
@@ -87,7 +87,7 @@ for ix = 1 : numel(lst)
     end
 end
 %}
-return
+% return
 
 %% look for char(0)
 %{
@@ -105,7 +105,7 @@ for ix = 1 : numel(lst)
     end
 end
 %}
-return
+% return
 
 %% look for )?
 %{
@@ -123,21 +123,53 @@ for ix = 1 : numel(lst)
     end
 end
 %}
-return
+% return
 
 
 %% look for mosw.isa
-%
+%{
 pattern = 'mosw\.isa';
 
 lst = irisroom.iris4oct.irisfulldirlist('files',true,'fileExt','.m');
 
+nfor = 0;
+
 for ix = 1 : numel(lst)
-    [flg, lines] = irisroom.iris4oct.isPatternInFile(lst{ix},pattern);
+    [flg, lines] = irisroom.iris4oct.isPatternInFile(lst{ix},pattern,nfor);
     if flg
         fprintf('\n[file]: %s\n\t[lines]:\n',lst{ix});
         for nx = 1 : length(lines.n)
             fprintf('\t%4d: %s\n',lines.n(nx),lines.str{nx});
+            for fx = 1 : nfor
+                fprintf('\t%4d: %s\n',lines.n(nx)+fx,lines.fwd{nx}{fx});
+            end
+        end
+    end
+end
+%}
+% return
+
+
+%% look for ismatlab
+%
+pattern = 'ismatlab';
+
+lst = irisroom.iris4oct.irisfulldirlist('files',true,'fileExt','.m');
+
+nfor = 2;
+
+for ix = 1 : numel(lst)
+    [flg, lines] = irisroom.iris4oct.isPatternInFile(lst{ix},pattern,nfor);
+    if flg
+        fprintf('\n[file]: %s\n\t[lines]:\n',lst{ix});
+        for nx = 1 : length(lines.n)
+            fprintf('\t%4d: %s\n',lines.n(nx),lines.str{nx});
+            for fx = 1 : nfor
+                fprintf('\t%4d: %s\n',lines.n(nx)+fx,lines.fwd{nx}{fx});
+            end
+            if fx > 0 && nx < length(lines.n)
+                fprintf('\t-----\n');
+            end
         end
     end
 end
