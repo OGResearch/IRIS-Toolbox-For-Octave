b200711055162a970d44708b3ec8ed1fa5feb984
sergey plotnikov <nul0m@users.noreply.github.com>
2014-07-14 20:22:19 +0200

    subsasgn access issue work around was commented out since it should not happen in IRIS and supposed to work like it's in Octave now

--- classlib/@tseries/subsasgn.m
+++ classlib/@tseries/subsasgn.m
@@ -67,12 +67,12 @@ switch S(1).type
             This.start = This.start + shift;
         end
     otherwise
-        if ~ismatlab && strcmp(S(1).type,'.') % workaround for Octaves' classdef subsasgn() access issue
-            This.(S(1).subs) = Y;
-        else
+%         if ~ismatlab && strcmp(S(1).type,'.') % workaround for Octaves' classdef subsasgn() access issue
+%             This.(S(1).subs) = Y;
+%         else
             % Give standard access to public properties.
             This = builtin('subsasgn',This,S,Y);
-        end
+%         end
 end
 
 end
